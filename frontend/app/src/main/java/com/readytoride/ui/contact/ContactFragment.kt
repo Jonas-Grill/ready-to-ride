0845da4b52b8308df9fc74d93b79d8b7576d8a65
Alexander Salzmann <47944950+Al3x1999@users.noreply.github.com>
2022-05-31 10:56:57 +0200

    onclick-contact

--- frontend/app/src/main/java/com/readytoride/ui/contact/ContactFragment.kt
+++ frontend/app/src/main/java/com/readytoride/ui/contact/ContactFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import com.readytoride.R
 
 class ContactFragment : Fragment() {
@@ -27,7 +29,22 @@ class ContactFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
         // TODO: Use the ViewModel
+
+        //Logik für Auslesen von Contact Formular
+
+        val btn_send = findViewById(R.id.sentButton) as Button
+        btn_send.setOnClickListener {
+            EditText textName = (EditText)findViewById(R.id.inputName);
+            textName.getText();
+            EditText textMail = (EditText)findViewById(R.id.inputMail);
+            textMail.getText();
+            EditText textMail = (EditText)findViewById(R.id.inputMessage);
+            textMessage.getText();
+        }
+
     }
+
+
 
     /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
