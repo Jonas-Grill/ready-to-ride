58630dae1abfbcb730777e9ad5c170879df4e409
Jonas-Grill <jonasb.grill@gmail.com>
2022-06-02 21:48:57 +0200

    Changed to https

--- backend/main.ts
+++ backend/main.ts
@@ -37,8 +37,8 @@ if (ENV === "dev") {
 } else if (ENV === "prod") {
     await app.listen({
         port: PORT,
-        secure: false,
-        // certFile: CERT_PATH,
-        // keyFile: KEY_PATH,
+        secure: true,
+        certFile: CERT_PATH,
+        keyFile: KEY_PATH,
     });
 }
