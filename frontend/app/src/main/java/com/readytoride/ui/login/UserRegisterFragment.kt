71c60f7a1bab137fb349b1a5ff8cb48068b3b7bb
Alexander Salzmann <47944950+Al3x1999@users.noreply.github.com>
2022-06-06 15:24:26 +0200

    navigation from registration to home

--- frontend/app/src/main/java/com/readytoride/ui/login/UserRegisterFragment.kt
+++ frontend/app/src/main/java/com/readytoride/ui/login/UserRegisterFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.readytoride.R
+import com.readytoride.ui.home.HomeFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,7 +37,12 @@ class UserRegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_register, container, false)
+        var view = inflater.inflate(R.layout.fragment_register, container, false)
+        view.findViewById<Button>(R.id.btn_finish).setOnClickListener {
+            var navRegister = activity as FragmentNavigation
+            navRegister.navigateFrag(HomeFragment(), false)
+        }
+        return view
     }
 
     companion object {
