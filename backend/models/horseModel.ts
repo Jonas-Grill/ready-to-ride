36639b5c43b02e1ee65a86c5ca3121dc715cdea5
Jonas-Grill <jonasb.grill@gmail.com>
2022-06-10 09:45:10 +0200

    Finished horse repo

--- backend/models/horseModel.ts
+++ backend/models/horseModel.ts
@@ -1,27 +1,14 @@
 import {Bson} from "../deps.ts"
-import {UserRole} from "../types/userRole.ts";
-import {Focus} from "../types/focus.ts";
 import {Proficiency} from "../types/proficiency.ts";
-import {NameSchema} from "../types/user.ts";
+import {Colour} from "../types/colour.ts";
 
-export default interface UserModel {
+export default interface HorseModel {
     _id?: Bson.ObjectId;
     name: string;
     age: number;
-    colour: C
+    colour: Colour;
+    difficultyLevel: Proficiency;
     profilePicture?: string;
     description?: string;
-    achievements?:	{
-        year: number;
-        name: string;
-    }[];
-    certificates?:	{
-        year: number;
-        name: string;
-    }[];
     pictures?: string[];
-    // User
-    weight?: number;
-    height?: number;
-    proficiency?: Proficiency;
 }
---
+++ backend/repositories/horseRepo.ts
@@ -0,0 +1,79 @@
+import {Bson} from "../deps.ts";
+import db from "../config/db-connection.ts";
+import Horse from "../models/horseModel.ts"
+import InvalidIdException from "../exceptions/invalidIdException.ts";
+
+const horses = db.collection<Horse>("horses");
+
+export const createHorse = async (horse: Horse) => {
+    const id = await horses.insertOne({
+        name: horse.name,
+        age: horse.age,
+        colour: horse.colour,
+        difficultyLevel: horse.difficultyLevel,
+        profilePicture: horse.profilePicture,
+        description: horse.description,
+        pictures: horse.pictures,
+    });
+
+    return id.toString();
+}
+
+export const findHorse = async () => {
+    return await horses.find({}).toArray();
+}
+
+export const findHorseById = async (id: string) => {
+    if (!Bson.ObjectId.isValid(id)) {
+        throw new InvalidIdException();
+    }
+
+    return await horses.findOne({
+        _id: new Bson.ObjectId(id),
+    });
+}
+
+export const findHorseByName = async (name: string) => {
+    return await horses.findOne({
+        name: name,
+    });
+}
+
+export const updateHorse = async (horse: Horse) => {
+    if (!horse._id) {
+        throw new InvalidIdException();
+    }
+
+    if (!Bson.ObjectId.isValid(horse._id.toString())) {
+        throw new InvalidIdException();
+    }
+
+    await horses.updateOne(
+        {
+            _id: new Bson.ObjectId(horse._id.toString()),
+        },
+        {
+            $set: {
+                name: horse.name,
+                age: horse.age,
+                colour: horse.colour,
+                difficultyLevel: horse.difficultyLevel,
+                profilePicture: horse.profilePicture,
+                description: horse.description,
+                pictures: horse.pictures,
+            }
+        },
+    );
+
+    return await findHorseById(horse._id.toString());
+}
+
+export const deleteHorse = async (id: string) => {
+    if (!Bson.ObjectId.isValid(id)) {
+        throw new InvalidIdException();
+    }
+
+    return await horses.deleteOne({
+        _id: new Bson.ObjectId(id),
+    });
+}
