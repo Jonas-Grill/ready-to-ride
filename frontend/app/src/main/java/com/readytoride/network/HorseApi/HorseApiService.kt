c4db9d3c9ccdf22d80e0ee474ae1b7317991872e
NiklasSchaefer <schaefer_niklas_99@web.de>
2022-06-13 15:12:16 +0200

    request update

--- frontend/app/src/main/java/com/readytoride/network/HorseApi/HorseApiService.kt
+++ frontend/app/src/main/java/com/readytoride/network/HorseApi/HorseApiService.kt
@@ -2,9 +2,10 @@ package com.readytoride.network.HorseApi
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
+import retrofit2.http.*
 
 private const val BASE_URL =
     "https://ready-to-ride-backend.tk/"
@@ -18,6 +19,9 @@ interface HorseApiService {
     @GET("horses")
     suspend fun getHorses(): List<HorseEntity>
 
+    @GET("horses/{id}")
+    suspend fun getHorseById(@Path("id") horseId: String): HorseEntity
+
     @GET("horses/races")
     suspend fun getHorseRaces(): List<String>
 
@@ -26,6 +30,15 @@ interface HorseApiService {
 
     @GET("horses/levels")
     suspend fun getHorseLevels(): List<String>
+
+    @POST("horses")
+    suspend fun postNewHorse(@Body requestBody: HorseEntity): HorseEntity
+
+    @PUT("horses/{id}")
+    suspend fun updateHorse(@Path("id") horseId: String): HorseEntity
+
+    @DELETE("horses/{id}")
+    suspend fun deleteHorse(@Path("id") horseId: String): String
 }
 
 object HorseApi {
--- frontend/app/src/main/java/com/readytoride/network/UserApi/UserApiService.kt
+++ frontend/app/src/main/java/com/readytoride/network/UserApi/UserApiService.kt
@@ -1,9 +1,14 @@
 package com.readytoride.network.UserApi
 
+import com.readytoride.network.HorseApi.HorseEntity
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
 
 private const val BASE_URL =
     "https://ready-to-ride-backend.tk/"
@@ -13,6 +18,35 @@ private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 private val retrofit = Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create(moshi)).baseUrl(BASE_URL).build()
 
 interface UserApiService {
+
+    @GET("users")
+    suspend fun getUsers(): List<UserEntity>
+
+    @GET("users/{id}")
+    suspend fun getUserById(@Path("id") userId: String): UserEntity
+
+    //TODO --> muss hier eine CalendarEntity erstellt werden?
+    @GET("users/{id}/calendar")
+    suspend fun getUserCalendar(): String
+
+    @GET("users/roles")
+    suspend fun getRoles(@Path("id") userId: String): List<String>
+
+    @GET("users/focuses")
+    suspend fun getFocuses(): List<String>
+
+    @GET("users/proficiencies")
+    suspend fun getProficiencies(): List<String>
+
+    @GET("users/me")
+    suspend fun getMyUser(): UserEntity
+
+    @POST("users")
+    suspend fun postNewUser(@Body requestBody: UserEntity): HorseEntity
+
+    @POST("users/login")
+    suspend fun login(@Body requestBody: LoginEntity): TokenEntity
+
 }
 
 object UserApi {
