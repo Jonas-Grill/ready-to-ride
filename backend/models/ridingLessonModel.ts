3457ae1ce69806d521dc0ce32d3b2106fc6ef6da
GinaWeber <Equus1904@gmx.de>
2022-06-16 16:55:24 +0200

    Update ridingLessonModel.ts

--- backend/models/ridingLessonModel.ts
+++ backend/models/ridingLessonModel.ts
@@ -1,7 +1,10 @@
-export interface RidingLessonSchema {
+import {Bson} from "../deps.ts";
+
+export default interface RidingLessonModel {
     _id?: Bson.ObjectId;
-    trainer?: string;
-    booked?: boolean;
+    trainer: string;
+    booked: boolean;
+    booker: string;
     arena: string;
     day: Date;
     startHour: number;
